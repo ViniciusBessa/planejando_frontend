fa35fcb82588cb8f19cbf7ca9cfd5d4d063d9e18
Vinícius Bessa <vinicius.b.silva3@gmail.com>
2023-02-06 16:17:52 -0300

    fix: os formulários dos componentes login, register e contact não exibiam um contorno vermelho nos campos inválidos

--- src/app/auth/password-reset/password-reset.component.ts
+++ src/app/auth/password-reset/password-reset.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.component.html',
-  styleUrls: ['./password-reset.component.css']
+  styleUrls: ['./password-reset.component.css'],
 })
-export class PasswordResetComponent {
-
-}
+export class PasswordResetComponent {}
