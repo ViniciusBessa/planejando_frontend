38e10130c6198180e340635591f85640cc1c847a
Vinícius Bessa <vinicius.b.silva3@gmail.com>
2023-02-18 16:19:31 -0300

    feat: realizado o deploy do projeto para o Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  apiUrl: '',
+  apiUrl: 'https://planejando-api.onrender.com/api/v1',
   production: true,
-  clientId: '',
+  clientId:
+    '497148625524-l14u7l9p0767govlfs273845cpa3on8f.apps.googleusercontent.com',
 };
